4f48370fd0e1c81cf0f1af376cee52960397a42b
jesus <jantolin@inhub.es>
2024-06-29 17:11:13 +0200

    dded configuration files and settings

---
+++ settings/config.py
@@ -0,0 +1,7 @@
+# Freelancer credentials
+FREELANCER_USER_NAME = "YourFreelancerUsername"
+FREELANCER_PASSWORD = "YourFreelancerPassword"
+
+# Chrome driver settings
+CHROME_VERSIONS = [90, 123]
+MAX_ATTEMPTS = 3
